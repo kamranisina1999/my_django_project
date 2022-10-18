1cc466d49332d8eea3ac3a95234c3a655da581ff
Sina Kamrani <kamrani.sina22@gmail.com>
2022-10-18 14:35:59 +0330

    add post model

--- new_apps/models.py
+++ new_apps/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField(blank=True)
+    view_count = models.IntegerField(null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
